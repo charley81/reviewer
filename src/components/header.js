db3f9dc0bd81b2cd98d9865267b9e47e2985f402
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-15 21:04:34 -0500

    created header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Reviewer</h1>
+      <div className="underline"></div>
+    </header>
+  )
+}
+
+export default Header
